f85ee2544f296455a6ca319bfac3867385f5ba28
BaNbO01 <bobantodic23@gmail.com>
2024-02-26 14:48:01 +0100

    Dodat BibliotekaInterfejs

---
+++ Biblioteka/src/interfejs/BibliotekaInterfejs.java
@@ -0,0 +1,13 @@
+package interfejs;
+
+import java.util.List;
+
+import biblioteka.Autor;
+import biblioteka.Knjiga;
+
+public interface BibliotekaInterfejs {
+	public void dodajKnjigu(Knjiga k);
+	public void obrisiKnjigu(Knjiga k);
+	public List<Knjiga> vratiSveKnjige();	
+	public List<Knjiga> pronadjiKnjigu(Autor autor,long isbn,String naslov, String izdavac);	
+}
