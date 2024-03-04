8dfa5e13f64a65c8b7790f88c2d7aef9b50ce07a
BaNbO01 <bobantodic23@gmail.com>
2024-03-04 12:56:55 +0100

    Dodata logicka kontrola u klasu Biblioteka

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -13,12 +13,20 @@ public class Biblioteka implements BibliotekaInterfejs  {
 	
 	@Override
 	public void dodajKnjigu(Knjiga k) {
+		if(k==null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		if(knjige.contains(k))
+			throw new IllegalArgumentException("Knjiga vec postoji u biblioteci");
 		knjige.add(k);
-		
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga k) {
+		if(k==null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		
+		if(!knjige.contains(k))
+			throw new IllegalArgumentException("Knjiga vec postoji u biblioteci");
 	knjige.remove(k);
 		
 	}
@@ -31,6 +39,10 @@ public class Biblioteka implements BibliotekaInterfejs  {
 
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		if(autor==null && isbn==0 && naslov==null && izdavac==null)
+			throw new IllegalArgumentException("Morate uneti bar jedan kriterujum");
+
+		
 		List<Knjiga> rezultati = new ArrayList<>();
 		for (Knjiga knjiga : rezultati) {
 			if(knjiga.getNaslov().toUpperCase().contains(naslov)) {
