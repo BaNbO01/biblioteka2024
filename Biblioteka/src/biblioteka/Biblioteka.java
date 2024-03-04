370b3389d5705cd84affd9020c0e1034b28135c9
BaNbO01 <bobantodic23@gmail.com>
2024-03-04 12:46:00 +0100

    Napravljena klasa Biblioteka koja implementira BibliotekaInterfejs

---
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,44 @@
+package biblioteka;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs  {
+
+	private List<Knjiga> knjige = new ArrayList<>();
+	
+	@Override
+	public void dodajKnjigu(Knjiga k) {
+		knjige.add(k);
+		
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga k) {
+	knjige.remove(k);
+		
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		// TODO Auto-generated method stub
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		List<Knjiga> rezultati = new ArrayList<>();
+		for (Knjiga knjiga : rezultati) {
+			if(knjiga.getNaslov().toUpperCase().contains(naslov)) {
+				rezultati.add(knjiga);
+			}
+		}
+		return rezultati;
+	}
+
+	
+}
